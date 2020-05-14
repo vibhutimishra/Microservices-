fd097125db36883cc0b6d46947e4bfacb6c2a581
vibhutimishra <53276136+vibhutimishra@users.noreply.github.com>
2020-05-14 06:30:20 +0530

    inventory microservice

--- app.py
+++ app.py
@@ -1,44 +1,32 @@
 from flask import Flask,jsonify
+import requests
 
 import os
 
 app = Flask(__name__)
 
-@app.route('/inventory')
-def inventory():
-    invent = {
-        "Brand": "BIOTRONIC",
-        "Longevity": "5 years",
+@app.route('/')
+def hello():
+    name = os.environ.get('MYAPP_NAME', 'MyApp')
+    print(name)
+    r={
+    "Brand": "BIOTRONIC",
+    "Longevity": "5 years",
 
 
-        "Weight": "21.5 grams",
+    "Weight": "21.5 grams",
 
-        "Size": "4.57*4.45*0.75 cm",
+    "Size": "4.57*4.45*0.75 cm",
 
-        "Voltage": "2.8 V",
+    "Voltage": "2.8 V",
 
 
-        "Average Projected Capacity" : "1.28 Ah"
-
+    "Average Projected Capacity" : "1.28 Ah"
     }
-
-    return jsonify(invent)
+    # extracting data in json format
+    return jsonify(r)
     # return '<br\><h3><center>Hello, Welcome to Dockerized Flask (' + name + ') !!!<center></h3>'
-
-@app.route('/monitor')
-def monitor():
-
-    monitor = {
-         "Status": "Active",
-         "Last Active":"23-02-2020",
-         "Packet Error Rate": "3%",
-         "Sensitivity ": "-123dB"
-    }
-    return jsonify(monitor)
-    # return '<br\><h3><center>Hello, Welcome to Dockerized Flask (' + name + ') !!!<center></h3>'
-
 
 if __name__ == '__main__':
-
-
-    app.run()
+    port = int(os.environ.get('MYAPP_PORT', 5001))
+    app.run(host='0.0.0.0', port=port)
